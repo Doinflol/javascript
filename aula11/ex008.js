985b12eba498985ab0c3340051f0e0f397be7fa8
Doinflol <112667210+Doinflol@users.noreply.github.com>
2022-09-01 20:37:08 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.
+console.log(`a velocidade do seu carro é ${vel}km/`)
+if (vel > 60) { //condição simples!
+    console.log(`Você ultrtapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
