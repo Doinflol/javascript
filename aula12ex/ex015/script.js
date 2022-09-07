c7415aa33fc6039edf90a5478b884f9d6797c483
Doinflol <112667210+Doinflol@users.noreply.github.com>
2022-09-07 00:45:32 -0300

    meio

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,18 +3,37 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('#res')
-    console.log (fano)
-    console.log (fano.value)
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano -  Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
-            genero = 'Homem'    
+            genero = 'Homem' 
+            if  (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'bebeh.png')
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //adulto
+            } else {
+                //Idoso
+            }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if  (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //adulto
+            } else {
+                //Idoso
+            }
         }
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
         }
