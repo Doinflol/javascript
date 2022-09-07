f533461e224cc281cddad4ebeb6cbb24c8888fb6
Doinflol <112667210+Doinflol@users.noreply.github.com>
2022-09-07 20:51:45 -0300

    atualização ex

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -2,14 +2,14 @@ function verificar(){
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.querySelector('#res')
+    var txt = document.querySelector('#txt')
+    var img = document.querySelector('#img')
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano -  Number(fano.value)
         var genero = ''
-        var img = document.createElement('img')
         img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem' 
@@ -42,8 +42,6 @@ function verificar(){
                 img.setAttribute('src', 'idosom.png')
             }
         }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        res.appendChild(img)
+        txt.innerText = `Detectamos ${genero} com ${idade} anos.`
         }
 }
