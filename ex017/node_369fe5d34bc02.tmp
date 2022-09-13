e941c094fe2ceb9705b31ecec5a12c3b7c35b74c
Doinflol <112667210+Doinflol@users.noreply.github.com>
2022-09-13 00:06:19 -0300

    meio de exxercício

---
+++ ex017/node_369fe5d34bc02.tmp
@@ -0,0 +1,6 @@
+var n = 1
+do {
+    console.log(`${n}`)
+    n * 2
+    n * 3
+} while (n <= n * 10)
--- ex017/script.js
+++ ex017/script.js
@@ -1,20 +1,10 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
-        //bom dia
-        img.src = 'manha.png'
-        document.body.style.background = '#a07841'
-    } else if (hora >= 12 && hora < 18) {
-        //boa tarde
-        img.src = 'tarde.png'
-        document.body.style.background = '#bed8e9'
-    } else {
-        //boa noite
-        img.src = 'noite.png'
-        document.body.style.background = '#403457'
-    }
-}
+function calcular()
+var n = document.querySelector('#txtn')
+var res = document.querySelector('#res')
+do {
+    n * 1
+    n * 2
+} while (n <= n * 10)
+
+
+res.innerHTML = 'Insira o número acima'
