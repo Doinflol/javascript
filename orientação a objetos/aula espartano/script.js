271cffcef9cf36775d37ab90dae0308212ca58c8
Doinflol <112667210+Doinflol@users.noreply.github.com>
2022-12-13 00:28:01 -0300

    att

---
+++ orientação a objetos/aula espartano/script.js
@@ -0,0 +1,17 @@
+class Produto {
+    constructor() {
+        this.id = 0;
+        this.nomeProduto - '';
+        this.valor - 0;
+    }
+
+    adicionar() {
+        alert('Vamos adicionar ao chamar!')
+    }
+
+    excluir() {
+        alert('Vamos excluir!')
+    }
+}
+
+var produto = new Produto()
